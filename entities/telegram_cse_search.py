6d45908815323351e78a2388b0bff31330be2048
jerlendds <jerlendds@proton.me>
2025-09-28 17:58:01 -0600

    rename telegram_websearch to telegram_cse_search and fix calls for new plugins api

--- entities/telegram_cse_search.py
+++ entities/telegram_cse_search.py
@@ -5,18 +5,13 @@ from osintbuddy.elements import TextInput
 class TelegramWebsearch(Plugin):
     version = "1.0.0"
  
-    label = "Telegram Websearch"
+    label = "Telegram CSE Search"
     author = "OSIB"
-    description = "Web search for telegram communities"
+    description = "CSE search for telegram communities"
 
     color = "#2AABEEaa"
     icon = "brand-telegram"
 
     elements = [
         TextInput(label="Query", icon="search")
-    ]
-
-    telegram_cse_urls = [
-        "https://cse.google.com/cse?&cx=006368593537057042503:efxu7xprihg",
-        "https://cse.google.com/cse?cx=004805129374225513871:p8lhfo0g3hg",
     ]
